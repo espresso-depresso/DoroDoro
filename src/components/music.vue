e9f5b21b6e4c95dce2bef652f731b78d831e0455
aleanny <aleanny.olivero@gmail.com>
2026-08-11 16:11:34 -0400

    otra vez

--- src/components/music.vue
+++ src/components/music.vue
@@ -2,7 +2,7 @@
 import { ref } from 'vue';
 import Swal from 'sweetalert2';
 
-const apikey=VITE_API_KEY_MUISC;
+const apikey=import.meta.env.VITE_API_KEY_MUISC;
 
 const song=ref('');
 const videoId = ref('');
