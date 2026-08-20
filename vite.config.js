a8fe51561e3b94bb085524d9ef60dc77751ff259
aleanny <aleanny.olivero@gmail.com>
2026-08-19 20:42:09 -0400

    again

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   base:'/DoroDoro/',
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(),vueDevTools(),],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
